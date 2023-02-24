56ea92c518a062a208d7a1adeb8a630ac393c645
Norman <whosnorm@gmail.com>
2023-02-24 14:02:39 -0500

    added render.yaml

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -38,7 +38,7 @@ const Item = ({ item, width }) => {
           alt={item.name}
           width="300px"
           height="400px"
-          src={`https://ecommerce-52pf.onrender.com${url}`}
+          src={`http://localhost:1337${url}`}
           onClick={() => navigate(`/item/${item.id}`)}
           style={{ cursor: "pointer" }}
         />
